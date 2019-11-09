20e189f986da222ea77cb8a6d29526f90c461c08
duongtu81390 <54531349+duongtu81390@users.noreply.github.com>
2019-11-09 16:12:38 +0700

    Add files via upload

--- src/app/changepw/changepw.component.ts
+++ src/app/changepw/changepw.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { StudentService } from '../DAL/student.service';
 
 @Component({
   selector: 'app-changepw',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangepwComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient, private stu: StudentService) { }
+  url = './assets/db/Students.json';
+  studentArray: any;
+  student:any;
+  userName: any;
+  Password:any;
+  nPassword: any;
+  cPassword: any;
   ngOnInit() {
-  }
+    this.getStudents().subscribe(data => {
+      this.studentArray = data;
+    })
+    this.student = this.stu.getStudent();
 
+  }
+  changePass() {
+    
+    for (let index = 0; index < this.studentArray.length; index++) {
+      if (this.nPassword != this.cPassword){
+        window.alert('Mật khẩu nhập lại chưa đúng! Vui lòng nhập lại.')
+        break}
+      if (this.userName === this.studentArray[index].username && this.Password === this.studentArray[index].password) {
+          this.studentArray[index].password=this.cPassword;
+          window.alert('Đã đổi mật khẩu.')
+      }
+    }
+
+  }
+  getStudents() {
+    return this.http.get(this.url)
+  }
 }
