5387ae6703c9da630f4e1bec63d0b54a32ee0208
duongtu81390 <54531349+duongtu81390@users.noreply.github.com>
2019-11-09 16:16:11 +0700

    Add files via upload

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-test',
@@ -7,117 +8,29 @@ import { ActivatedRoute } from '@angular/router'
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  subject = [
-    {
-      "Id": "ADAV",
-      "Name": "Lập trình Android nâng cao",
-      "Logo": "ADAV.jpg"
-    },
-    {
-      "Id": "ADBS",
-      "Name": "Lập trình Android cơ bản",
-      "Logo": "ADBS.jpg"
-    },
-    {
-      "Id": "ADTE",
-      "Name": "Kiểm thử và triển khai ứng dụng Android",
-      "Logo": "ADTE.jpg"
-    },
-    {
-      "Id": "ADUI",
-      "Name": "Thiết kế giao diện trên Android",
-      "Logo": "ADUI.jpg"
-    },
-    {
-      "Id": "ASNE",
-      "Name": "Lập trình ASP.NET",
-      "Logo": "ASNE.png"
-    },
-    {
-      "Id": "CLCO",
-      "Name": "Điện toán đám mây",
-      "Logo": "CLCO.jpg"
-    },
-    {
-      "Id": "DBAV",
-      "Name": "SQL Server",
-      "Logo": "DBAV.png"
-    },
-    {
-      "Id": "DBBS",
-      "Name": "Cơ sở dữ liệu",
-      "Logo": "DBBS.png"
-    },
-    {
-      "Id": "GAME",
-      "Name": "Lập trình game 2D",
-      "Logo": "GAME.png"
-    },
-    {
-      "Id": "HTCS",
-      "Name": "HTML5 và CSS3",
-      "Logo": "HTCS.jpg"
-    },
-    {
-      "Id": "INMA",
-      "Name": "Internet Marketing",
-      "Logo": "INMA.jpg"
-    },
-    {
-      "Id": "JAAV",
-      "Name": "Lập trình Java nâng cao",
-      "Logo": "JAAV.png"
-    },
-    {
-      "Id": "JABS",
-      "Name": "Lập trình hướng đối tượng với Java",
-      "Logo": "JABS.png"
-    },
-    {
-      "Id": "JSPR",
-      "Name": "Lập trình JavaScript",
-      "Logo": "JSPR.png"
-    },
-    {
-      "Id": "LAYO",
-      "Name": "Thiết kế layout",
-      "Logo": "LAYO.jpg"
-    },
-    {
-      "Id": "MOWE",
-      "Name": "Thiết kế web cho điện thoại di động",
-      "Logo": "MOWE.png"
-    },
-    {
-      "Id": "PHPP",
-      "Name": "Lập trình PHP",
-      "Logo": "PHPP.png"
-    },
-    {
-      "Id": "PMAG",
-      "Name": "Quản lý dự án với Agile",
-      "Logo": "PMAG.jpg"
-    },
-    {
-      "Id": "VBPR",
-      "Name": "Lập trình VB.NET",
-      "Logo": "VBPR.png"
-    },
-    {
-      "Id": "WEBU",
-      "Name": "Xây dựng trang web",
-      "Logo": "WEBU.jpg"
-    }
-  ]
+  url='../../assets/db/Subjects.js'
+  subject:any;
+  
   ipp = 4;
   cp = 1;
   totalPage: number;
-  pId;
-  constructor(private route: ActivatedRoute) { }
+  
+  constructor( private http: HttpClient) { }
 
 
   ngOnInit() {
-    this.totalPage = Math.ceil(this.subject.length / this.ipp)
+    this.getSubject().subscribe(data => 
+      {
+        this.subject=data;
+        
+      }
+      )
+      
+    this.totalPage = Math.ceil(20 / this.ipp)
+  }
+  getSubject()
+  {
+    return this.http.get(this.url);
   }
   Next() {
     if (this.cp < this.totalPage) {
