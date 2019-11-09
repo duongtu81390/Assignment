974b754ab552d919cfb86754a5e713cd0896ea4a
duongtu81390 <54531349+duongtu81390@users.noreply.github.com>
2019-11-09 16:14:26 +0700

    Add files via upload

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { StudentService } from '../DAL/student.service';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-
-  constructor() { }
-
+  check:boolean=false;
+  constructor(private http: HttpClient,private stu: StudentService) { }
+  userNameToCheck: any;
+  passwordToCheck: any;
+  studentArray: any;
+  student: any
+  url = './assets/db/Students.js';
   ngOnInit() {
+    this.getStudents().subscribe(data =>{
+      this.studentArray = data;
+    })
+    this.student = this.stu.getStudent();
   }
+  getStudents(){
+    return this.http.get(this.url)
+  }
+  checkLogin(){
+    for(var student of this.studentArray){
+      if((this.userNameToCheck === student.username) && (this.passwordToCheck === student.password)){
+        this.check = true
+      }
+    }
+  }
+
 
 }
